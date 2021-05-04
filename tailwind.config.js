b5d0a9481fa03c1c9143c0f63c4f085db0f20658
Sergio Linares <sergiolinmar@gmail.com>
2021-05-04 21:16:30 +0200

    Add Tailwind initial config

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+module.exports = {
+    prefix: '',
+    purge: {
+      enabled: guessProductionMode(),
+      content: [
+        './src/**/*.{html,ts}',
+      ]
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        colors: {
+          grey: {
+            light: '#b5b5b5',
+            'extra-light': '#f4f4f4',
+            dark: '#707070',
+            'extra-dark': '#3F3F3F'
+          },
+          green: '#5EBEA3',
+          salmon: '#FF9984'
+        }
+      }
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms'),require('@tailwindcss/line-clamp'),require('@tailwindcss/typography')],
+};
