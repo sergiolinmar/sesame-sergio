f02d830ee71698019c93028304e2aea6a8503bb3
Sergio Linares <sergiolinmar@gmail.com>
2021-05-05 21:00:51 +0200

    Add components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimeTrackerPageComponent } from './pages/time-tracker-page/time-tracker-page.component';
+import { TimeTrackerComponent } from './pages/time-tracker-page/components/time-tracker/time-tracker.component';
+import { TimeTrackerUserComponent } from './pages/time-tracker-page/components/time-tracker-user/time-tracker-user.component';
+import { UserAvatarComponent } from './shared/components/user-avatar/user-avatar.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimeTrackerPageComponent
+    TimeTrackerPageComponent,
+    TimeTrackerComponent,
+    TimeTrackerUserComponent,
+    UserAvatarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/time-tracker-page/components/time-tracker-user/time-tracker-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-tracker-user',
+  templateUrl: './time-tracker-user.component.html',
+  styleUrls: ['./time-tracker-user.component.scss']
+})
+export class TimeTrackerUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
